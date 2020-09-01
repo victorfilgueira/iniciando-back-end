ea7878dff12ccd81ed510c25b2c6b43b1700b517
victorfilgueira <victorfilgueira@id.uff.br>
2020-09-01 14:25:23 -0300

    error create user

---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user')
+class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    // eslint-disable-next-line camelcase
+    created_at: Date;
+
+    @UpdateDateColumn()
+    // eslint-disable-next-line camelcase
+    updated_at: Date;
+}
+
+export default User;
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,39 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+    provider: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(
+            AppointmentsRepository,
+        );
+
+        const appointmentDate = startOfHour(date);
+
+        const findAppointmentAtTheSameDate = await appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (findAppointmentAtTheSameDate) {
+            throw Error('This appointment is already booked');
+        }
+
+        const appointment = appointmentsRepository.create({
+            provider,
+            date: appointmentDate,
+        });
+
+        await appointmentsRepository.save(appointment);
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
